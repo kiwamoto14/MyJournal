fe33b1a1e2299e12b78e8a66482f7b48b2ad464a
Kazushi Iwamoto <kazu4az@gmail.com>
2020-06-13 13:53:54 -0700

    Initial Commit

---
+++ MyJournal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyJournal
+//
+//  Created by Kazushi Iwamoto on 6/13/20.
+//  Copyright © 2020 Kazushi Iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyJournal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyJournal
+//
+//  Created by Kazushi Iwamoto on 6/13/20.
+//  Copyright © 2020 Kazushi Iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
